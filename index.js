fa6f61c0d7771c348bf4564bf05f286f86fb9b61
AbdullahWB <abdullah917828@gmail.com>
2023-05-13 21:30:35 +0600

    make update option

--- index.js
+++ index.js
@@ -61,8 +61,17 @@ async function run() {
       res.send(result);
     })
 
-    app.put('/bookings/:id', async (req, res) => { 
+    app.patch('/bookings/:id', async (req, res) => { 
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updatedBooking = req.body;
+      const updatedDoc = {
+        $set: {
+          status: updatedBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     })
 
     app.delete('/bookings/:id', async (req, res) => { 
